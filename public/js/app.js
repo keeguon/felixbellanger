bca13cbf0a8a59cb57a4df1db737ae87ffe88f64
Félix Bellanger <felix.bellanger@gmail.com>
2011-05-24 23:17:05 +0200

    First experiments loading content in pages w/out reloading and using transitions to smooth the darn thing a lil' bit

--- public/js/app.js
+++ public/js/app.js
@@ -9,21 +9,45 @@ var app = Davis(function() {
   });
 
   this.get('/blog', function(req) {
-    $.getJSON(req.path, function(res) {
-      console.log(res);
-    });
+    if (!req.isForPageLoad) {
+      $.getJSON(req.path, function(res) {
+        $('#main').animate({ left: '+=960' }, function() {
+          
+          var source = $('<div/>').html($('#posts-index').html()).text();
+          var template = Handlebars.compile(source);
+
+          $('#main').css({ left: '-960px' }).empty();
+          $('#main').append(template(res));
+          $('#main').animate({ left: '+=960' });
+
+        });
+      });
+    }
   });
 
   this.get('/blog/:year/:month/:day/:slug', function(req) {
-    $.getJSON(req.path, function(res) {
-      console.log(res);
-    });
+    if (!req.isForPageLoad) {
+      $.getJSON(req.path, function(res) {
+        $('#main').animate({ left: '-=960' }, function() {
+          
+          var source = $('<div/>').html($('#posts-post').html()).text();
+          var template = Handlebars.compile(source);
+
+          $('#main').css({ left: '960px' }).empty();
+          $('#main').append(template(res));
+          $('#main').animate({ left: '-=960' });
+
+        });
+      });
+    }
   });
 
   this.get('/about', function(req) {
-    $.getJSON(req.path, function(res) {
-      console.log(res);
-    });
+    if (req.isPageForLoad == false) {
+      $.getJSON(req.path, function(res) {
+        console.log(res);
+      });
+    }
   });
 
 });
