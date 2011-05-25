faac845cbd2ef5a5a720cba0481adb650164aa14
Félix Bellanger <felix.bellanger@gmail.com>
2011-05-25 15:58:56 +0200

    Fixed issues w/ missing templates in static pages

--- app/views/pages.rb
+++ app/views/pages.rb
@@ -4,6 +4,16 @@ class FelixBellanger::Pages
       def self.template_file
         File.join(File.dirname(__FILE__), '../templates/layout.mustache')
       end
+
+      def template_tags
+        html = ''
+        %w{pages/page posts/index posts/post}.each do |template|
+          template_file = File.open(File.join(File.dirname(__FILE__), '..', 'templates', "#{template}.mustache"), "rb")
+          template_name = template.sub(/\//, '-')
+          html = html + "<script type=\"text/x-mustache-template\" id=\"#{template_name}\">\r\n" + CGI::escapeHTML(template_file.read) + "</script>\r\n"
+        end
+        return html
+      end
     end
 
     class Page < Layout
